02f6a52164c1f5a118f87203518814d36f9b8ab4
Brian Donaghey <briandonaghey21@gmail.com>
2025-03-31 21:58:11 -0400

    Add react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content">
+        <div className="container">
+          <h1 className="hello">
+            Hello! <span className="wave">👋</span>
+          </h1>
+          <h1>I'm Brian Donaghey</h1>
+          <p>
+            I am currently a Senior at the <b>University of Pittsburgh</b> studying Computer Science with a minor in Economics.
+            My primary interests lie in <b>full-stack</b> and <b>back-end</b> development. More specifically, I am passionate about
+            <b> software development, data engineering, software quality assurance, database management, and artificial intelligence</b>.
+            With graduation approaching, I am currently seeking full-time roles in a position where I am challenged and actively learning new things every day.
+          </p>
+
+          <div className="nav-cards">
+          <a href="index.html" className="nav-card">Home</a>
+         {/* <a href="aboutMe.html" className="nav-card">About Me</a> */}
+          <a href="../assets/Brian_Donaghey_Resume.pdf" className="nav-card">Resume</a>
+
+          <a href="https://www.linkedin.com/in/brian-donaghey/" className="nav-card with-icon">
+            <img src="../assets/linkedin.png" alt="LinkedIn" className="icon" />
+            LinkedIn
+          </a>
+
+          <a href="https://github.com/briandonaghey21" className="nav-card with-icon">
+            <img src="../assets/github.png" alt="GitHub" className="icon" />
+            GitHub
+          </a>
+          </div>
+
+
+        </div>
+
+        <img src="../assets/brian.png" alt="Brian" className="Brian" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
